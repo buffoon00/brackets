79eae2b08b89d7de37e87b1d7a2a440b696ae1d7
buffoon00 <todorovich.ute@gmail.com>
2022-08-01 21:37:36 +0300

    feat:solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const bracketsPair = {};
+  for (i = 0; i < bracketsConfig.length; i++){
+    const key = bracketsConfig[i][1];
+    const value = bracketsConfig[i][0];
+    openBrackets.push(value);
+    bracketsPair[key] = value;
+  }
+
+  let stack = [];
+    for (i = 0; i < str.length; i++) {
+      let currentSymb = str[i];
+      let topElem = stack[stack.length - 1];
+      if (stack.length !== 0 && bracketsPair[currentSymb] === topElem) {
+        stack.pop();
+      } else if (openBrackets.includes(currentSymb)){
+        stack.push(currentSymb);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+         else if (bracketsPair[currentSymb] === topElem) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }      
+    }     
+  return stack.length === 0;
 }
